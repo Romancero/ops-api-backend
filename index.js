075bd1a06841c1c9932079e19732121a87cb2515
Roman Med <romanmedinaceli2004@gmail.com>
2022-10-18 15:53:49 -0300

    API 1.0.0 - school-news | API 1.0.0 - school-events

--- index.js
+++ index.js
@@ -43,6 +43,102 @@ let pages = [
   },
 ];
 
+//new Date().toISOString()
+
+let schoolEvents = [
+  {
+    id: 1,
+    title: 'Fiesta de Egresados PROMO-22',
+    startDate: '2019-05-30T18:39:34.091Z',
+    finishDate: '2019-05-30T18:39:34.091Z',
+    description: ['Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'],
+    schools: ['E.E.S.T. N°14', 'E.E.S.T. N°7', 'E.E.S.T. N°3'],
+    organization: ['The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+    blocks: ['Morbi ut metus eu sem dapibus mattis.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Praesent accumsan neque eget erat mollis, eget dictum felis porta.'],
+    blocksDescription: ['Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+  },
+  {
+    id: 2,
+    title: 'Fiesta de Primavera',
+    startDate: '2019-05-30T18:39:34.091Z',
+    finishDate: '2019-05-30T18:39:34.091Z',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+    schools: ['E.E.S.T. N°14', 'E.E.S.T. N°7', 'E.E.S.T. N°3'],
+    organization: ['The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+    blocks: ['Morbi ut metus eu sem dapibus mattis.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Praesent accumsan neque eget erat mollis, eget dictum felis porta.'],
+    blocksDescription: ['Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+  },
+  {
+    id: 3,
+    title: 'Acto del Día Nacional de la Memoria por la Verdad y la Justicia',
+    startDate: '2019-05-30T18:39:34.091Z',
+    finishDate: '2019-05-30T18:39:34.091Z',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+    schools: ['E.E.S.T. N°14', 'E.E.S.T. N°7', 'E.E.S.T. N°3'],
+    organization: ['The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+    blocks: ['Morbi ut metus eu sem dapibus mattis.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Praesent accumsan neque eget erat mollis, eget dictum felis porta.'],
+    blocksDescription: ['Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+  },
+  {
+    id: 4,
+    title: 'Acto del Día del Veterano y de los Caídos en la Guerra de Malvinas',
+    startDate: '2019-05-30T18:39:34.091Z',
+    finishDate: '2019-05-30T18:39:34.091Z',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+    schools: ['E.E.S.T. N°14', 'E.E.S.T. N°7', 'E.E.S.T. N°3'],
+    organization: ['The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+    blocks: ['Morbi ut metus eu sem dapibus mattis.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Praesent accumsan neque eget erat mollis, eget dictum felis porta.'],
+    blocksDescription: ['Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+  }
+];
+
+let schoolNews = [
+  {
+    id: 1,
+    title: 'Fiesta de Egresados PROMO-22',
+    startDate: '2019-05-30T18:39:34.091Z',
+    finishDate: '2019-05-30T18:39:34.091Z',
+    description: ['Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'],
+    schools: ['E.E.S.T. N°14', 'E.E.S.T. N°7', 'E.E.S.T. N°3'],
+    organization: ['The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+    blocks: ['Morbi ut metus eu sem dapibus mattis.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Praesent accumsan neque eget erat mollis, eget dictum felis porta.'],
+    blocksDescription: ['Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+  },
+  {
+    id: 2,
+    title: 'Fiesta de Primavera',
+    startDate: '2019-05-30T18:39:34.091Z',
+    finishDate: '2019-05-30T18:39:34.091Z',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+    schools: ['E.E.S.T. N°14', 'E.E.S.T. N°7', 'E.E.S.T. N°3'],
+    organization: ['The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+    blocks: ['Morbi ut metus eu sem dapibus mattis.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Praesent accumsan neque eget erat mollis, eget dictum felis porta.'],
+    blocksDescription: ['Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+  },
+  {
+    id: 3,
+    title: 'Acto del Día Nacional de la Memoria por la Verdad y la Justicia',
+    startDate: '2019-05-30T18:39:34.091Z',
+    finishDate: '2019-05-30T18:39:34.091Z',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+    schools: ['E.E.S.T. N°14', 'E.E.S.T. N°7', 'E.E.S.T. N°3'],
+    organization: ['The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+    blocks: ['Morbi ut metus eu sem dapibus mattis.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Praesent accumsan neque eget erat mollis, eget dictum felis porta.'],
+    blocksDescription: ['Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+  },
+  {
+    id: 4,
+    title: 'Acto del Día del Veterano y de los Caídos en la Guerra de Malvinas',
+    startDate: '2019-05-30T18:39:34.091Z',
+    finishDate: '2019-05-30T18:39:34.091Z',
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+    schools: ['E.E.S.T. N°14', 'E.E.S.T. N°7', 'E.E.S.T. N°3'],
+    organization: ['The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+    blocks: ['Morbi ut metus eu sem dapibus mattis.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Praesent accumsan neque eget erat mollis, eget dictum felis porta.'],
+    blocksDescription: ['Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'],
+  }
+];
+
 app.get('/', (request, response) => {
   response.send('<h1>Welcome to the site!</h1>');
 });
@@ -57,6 +153,40 @@ app.get('/api/pages/:id', (request, response) => {
 
   if (page) {
     response.json(page);
+  } else {
+    response.status(404).json({
+      message: 'Page not found',
+    });
+  }
+});
+
+app.get('/api/school-events', (request, response) => {
+  response.json(schoolEvents);
+});
+
+app.get('/api/school-events/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const schoolEvent = schoolEvents.find(schoolEvent => schoolEvent.id === id);
+
+  if (schoolEvent) {
+    response.json(schoolEvent);
+  } else {
+    response.status(404).json({
+      message: 'Page not found',
+    });
+  }
+});
+
+app.get('/api/school-news', (request, response) => {
+  response.json(schoolNews);
+});
+
+app.get('/api/school-news/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const schoolNew = schoolNews.find(schoolNew => schoolNew.id === id);
+
+  if (schoolNew) {
+    response.json(schoolNew);
   } else {
     response.status(404).json({
       message: 'Page not found',
